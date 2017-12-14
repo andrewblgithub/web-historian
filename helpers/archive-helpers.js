a8f38d3fd633410f9e95c51d2f143f02f5b283e0
andrewblgithub <andrewblichtenstein@gmail.com>
2017-12-13 20:17:00 -0500

    Archive helpers in progress

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
+var http = require('http');
 var path = require('path');
+var wget = require('node-wget');
+var scraper = require('website-scraper');
 var _ = require('underscore');
 
 /*
@@ -26,16 +29,94 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  fs.readFile(exports.paths.list, 'utf-8', function(err, data) {
+    if (err) {
+      throw err;
+    } else {
+      callback(data.split('\n'));
+    }    
+  });
 };
 
 exports.isUrlInList = function(url, callback) {
+  exports.readListOfUrls((list) => {
+    if (list.indexOf(url) !== -1) {
+      callback(true)
+    } else {
+      exports.addUrlToList(url, () => {})
+      callback(false)
+    }
+  })
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.appendFile(exports.paths.list, '\n' + url, 'utf-8', function(err) {
+    if (err) {
+      throw err;
+    } else {
+      callback();
+    }
+  });
 };
 
 exports.isUrlArchived = function(url, callback) {
+  fs.readdir(exports.paths.archivedSites, (err, files) => {
+    for (var file of files) {
+      if (file === url) {
+        return true;
+      }
+    }
+    return false;
+  });
 };
 
 exports.downloadUrls = function(urls) {
+//wget -E -H -k -K -nd -N -p -P test \  https://www.npmjs.com/package/node-cron
+  urls.forEach(url => { 
+    if (!exports.isUrlArchived(url)) {
+      wget({
+        url: url,
+        dest: path.join(exports.paths.archivedSites, './' , url),      // destination path or path with filenname, default is ./ 
+        timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds 
+      },
+      function (error, response, body) {
+        if (error) {
+          console.log('--- error:');
+          console.log(error);            // error encountered 
+        } else {
+          // console.log('--- headers:');
+          // console.log(response.headers); // response headers 
+          // console.log('--- body:');
+          // console.log(body);             // content of package 
+        }
+      }
+      );
+    }
+  });
+
+  // console.log(scraper);
+
+  // var options = {
+  //   urls: ['www.google.com'],
+  //   directory: exports.paths.archivedSites
+  // };
+  
+  // scraper.scrape(options).then(function (result) {
+  //   console.log(result);
+  // });
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,50 @@
 var path = require('path');
+var url = require('url');
 var archive = require('../helpers/archive-helpers');
+var httpHelper = require('./http-helpers.js');
+var headers = Object.assign({}, httpHelper.headers);
+var validUrl = require('valid-url');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // routes and options based on them
+  // if req.url is / then serve up index in /public/index.html
+  console.log('serving request: ' + req.method + ' for: ' + req.url);
+  var q = url.parse(req.url);
+  if (req.method === 'GET') {
+    if (q.pathname === '/') {
+    // console.log(path.join(archive.paths.siteAssets, '../index.html'))
+      console.log('hi', archive.paths.archivedSites);
+      httpHelper.serveAssets(res, path.join(archive.paths.siteAssets, '../public/index.html'), function(data) {
+        res.writeHead(200, httpHelper.headers);
+        res.end(data);
+      });
+    } else if (q.pathname === '/styles.css') {
+      httpHelper.serveAssets(res, path.join(archive.paths.siteAssets, '../public/styles.css'), function(data) {
+        var newHeaders = Object.assign({}, httpHelper.headers);
+        newHeaders['Content-Type'] = 'text/css';
+        res.writeHead(200, newHeaders);
+        res.end(data);
+      });
+    } else if (q.pathname === 'doody') {
+      
+    }
+  } else if (req.method === 'POST') {
+    var userInput = '';
+    req.on('data', function(chunk) {
+      userInput += chunk.slice(4);
+    });
+    req.on('end', function() {
+      archive.isUrlInList(userInput, (isInside) => {
+        if (isInside) {
+          console.log('is inside');
+        } else {
+          console.log('is added');
+        }
+      });
+      res.writeHead(301, {Location: 'localhost:3000'});
+      res.end();
+    });
+  }
+  // res.end(archive.paths.list);
 };
